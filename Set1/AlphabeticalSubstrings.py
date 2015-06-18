f65cbde18d360cd2f8500617127251599f8517ba
YangVincent <VincentYang2014@gmail.com>
2015-06-17 17:51:47 -0700

    Cleaned up code

--- Set1/AlphabeticalSubstrings.py
+++ Set1/AlphabeticalSubstrings.py
@@ -1,12 +1,3 @@
-
-#s = 'kocibsxkbec' #bsx
-#s = 'ggnlfdktaatxv' #aatx
-#s = 'ioownrsocumeshalkrytyvs' #ioow
-#s = 'ytjaakglkb' #
-#s = 'deigapgfsfmuxqpapfbfra' #dei
-s = 'abcdefghijklmnopqrstuvwxyz'
-print('s is ' + s)
-#print("Hello World")
 counter = 2
 biggest = 2
 length = len(s) - 1
@@ -22,7 +13,5 @@ for i in range(length):
   else:
     counter = 2
     currentString = s[i+1]
-#print("The biggest is " + str(biggest))
-#print("The longest string is " + longestString)
 print('counter is ' + str(counter))
 print(longestString)
--- Set1/CountingBobs.py
+++ Set1/CountingBobs.py
@@ -1,21 +1,13 @@
-
-
-
-#print "Hello WOrld"
-#s = 'azcbobobegghakl'
-s = 'uoobbobbqbobobdoboosf'
-#print s.count('bob')
 count = 0
 while s.find('bob', 0, len(s)) != -1:
   if s.find('bob', 0, len(s)) != -1:
     count = count + 1
-    print 'count is now ' + str(count)
+    #print 'count is now ' + str(count)
     i = s.find('bob', 0, len(s))
-    print 'replacing' + s[i]
-#    s = s.remove(s[i])
-    print 'before replace s is ' + s
+    #print 'replacing' + s[i]
+    #print 'before replace s is ' + s
     s = s[:i] + s[i+2:]
 #    print 'after replace s is ' + s    
-    print 'in loop i is ' + str(i)
+#    print 'in loop i is ' + str(i)
 #print 'count is ' + str(count)
 print str(count)
